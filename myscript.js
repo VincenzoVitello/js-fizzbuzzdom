88bf4383e13e0255eab7d62fb814e770fc62250d
VincenzoVitello <92366183+VincenzoVitello@users.noreply.github.com>
2021-12-10 17:48:58 +0100

    progetto finito

---
+++ myscript.js
@@ -0,0 +1,16 @@
+let scatola = document.getElementById ('container'); //creo una variabile con potrò richiamare il mio container
+//scatola.innerHTML += '<div class="box">' + number + '</div>';
+
+for (let number = 1; number <= 100; number++) { //creato ciclo for
+    if(number % 3 == 0 && number % 5 == 0){
+        scatola.innerHTML += '<div class="box fizzbuzz">' + 'FizzBuzz' + '</div>' //if basato sui multipli sia di 3 che di 5
+    }else if (number % 3 == 0){
+        scatola.innerHTML += '<div class="box fizz">' + 'Fizz' + '</div>' //else if basato sui multipli di 3
+    }else if(number % 5 == 0){
+        scatola.innerHTML += '<div class="box buzz">' + 'Buzz' + '</div>' //else if basato sui multipli di 5
+    }else{
+        scatola.innerHTML += '<div class="box">' + number + '</div>' //altrimenti mostrare semplicemente il numero
+    }
+    
+}
+
